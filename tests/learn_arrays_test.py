31eaf4bfb8edaf78ed9a7d03b6607d5210d87f99
Stas Chereshkevich <chereshkevichsv@gmail.com>
2020-04-12 16:22:13 +0300

    Fix

--- tests/learn_arrays_test.py
+++ tests/learn_arrays_test.py
@@ -12,18 +12,18 @@ class LearnArraysTest(unittest.TestCase):
         array = [1, 2, 3, 4, 5]
         self.assertEqual(arrays.reverse(array), [5, 4, 3, 2, 1])
 
-    def five_index(self):
+    def test_five_index(self):
         array = [1, 2, 3, 4, 5]
         new_array = [5, 3, 8, 5, 7, 12]
         self.assertEqual(arrays.five_index(array), 4)
         self.assertEqual(arrays.five_index(new_array), 0)
 
-    def add_before_second(self):
+    def test_add_before_second(self):
         array = [1, 2, 3, 4, 5]
         new_item = 43
         self.assertEqual(arrays.add_before_second(array, new_item), [1, 43, 2, 3, 4, 5])
 
-    def remove_duplicates(self):
+    def test_remove_duplicates(self):
         array = [1, 2, 2, 3, 4, 5, 2, 1, 6, 7, 88, 8, 2]
         self.assertEqual(arrays.remove_duplicates(array), [1, 2, 3, 4, 5, 6, 7, 88, 8])
 
