37e3d3b0d93bc1a935146580d3d035e41b6bf0a4
Stas Chereshkevich <chereshkevichsv@gmail.com>
2020-04-12 16:08:26 +0300

    Arrays leasons

---
+++ src/learn/arrays.py
@@ -0,0 +1,23 @@
+def add(array, item):
+    # code here
+    return 0
+
+
+def reverse(array):
+    # code here
+    return 0
+
+
+def five_index(array):
+    # code here
+    return 0
+
+
+def add_before_second(array, item):
+    # code here
+    return 0
+
+
+def remove_duplicates(array):
+    # code here
+    return 0
---
+++ tests/learn_arrays_test.py
@@ -0,0 +1,29 @@
+from src.learn import arrays
+import unittest
+
+
+class LearnArraysTest(unittest.TestCase):
+    def test_add(self):
+        array = [1, 2, 3, 4, 5]
+        new_item = 6
+        self.assertEqual(arrays.add(array, new_item), [1, 2, 3, 4, 5, 6])
+
+    def test_reverse(self):
+        array = [1, 2, 3, 4, 5]
+        self.assertEqual(arrays.reverse(array), [5, 4, 3, 2, 1])
+
+    def five_index(self):
+        array = [1, 2, 3, 4, 5]
+        new_array = [5, 3, 8, 5, 7, 12]
+        self.assertEqual(arrays.five_index(array), 4)
+        self.assertEqual(arrays.five_index(new_array), 0)
+
+    def add_before_second(self):
+        array = [1, 2, 3, 4, 5]
+        new_item = 43
+        self.assertEqual(arrays.add_before_second(array, new_item), [1, 43, 2, 3, 4, 5])
+
+    def remove_duplicates(self):
+        array = [1, 2, 2, 3, 4, 5, 2, 1, 6, 7, 88, 8, 2]
+        self.assertEqual(arrays.remove_duplicates(array), [1, 2, 3, 4, 5, 6, 7, 88, 8])
+
